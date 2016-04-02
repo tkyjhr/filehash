48e2c1f83f390c4ad34be2c79546b9c0f16a5820
tkyjhr <tkyjhr@gmail.com>
2016-04-02 18:54:17 +0900

    Initial commit.

---
+++ cmd/filehash/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/tkyjhr/filehash"
+)
+
+func hashPrintCommand(name string, aliases []string, hashFunc func(file string) []byte) cli.Command {
+	return cli.Command{
+		Name:    name,
+		Aliases: aliases,
+		Action: func(c *cli.Context) {
+			files := c.Args()
+			for _, f := range files {
+				h := hashFunc(f)
+				fmt.Printf("%x\n", h)
+			}
+		},
+		Usage:     fmt.Sprintf("Print %s value(s) of [FILE]...", name),
+		ArgsUsage: "[FILE]...",
+	}
+}
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "filehash"
+	app.Usage = "Print hash value(s) of [FILE]..."
+	app.ArgsUsage = "[FILE]..."
+	app.UsageText = "filehash [COMMAND] [FILE]..."
+
+	commands := []cli.Command{
+		hashPrintCommand("SHA1", []string{"sha1", "s1"}, filehash.SHA1),
+		hashPrintCommand("SHA256", []string{"sha256"}, filehash.SHA256),
+		hashPrintCommand("SHA256-224", []string{"sha256-224"}, filehash.SHA224),
+		hashPrintCommand("SHA512", []string{"sha512"}, filehash.SHA512),
+		hashPrintCommand("SHA512-224", []string{"sha512"}, filehash.SHA512_224),
+		hashPrintCommand("SHA512-256", []string{"sha512"}, filehash.SHA512_256),
+		hashPrintCommand("SHA3-224", []string{"sha3-224", "s3-224"}, filehash.SHA3_224),
+		hashPrintCommand("SHA3-256", []string{"sha3-256", "s3-256"}, filehash.SHA3_256),
+		hashPrintCommand("SHA3-384", []string{"sha3-384", "s3-384"}, filehash.SHA3_384),
+		hashPrintCommand("SHA3-512", []string{"sha3-512", "s3-512", "s3"}, filehash.SHA3_512),
+		hashPrintCommand("MD5", []string{"md5", "m5"}, filehash.MD5),
+		hashPrintCommand("CRC32-IEEE", []string{"crc32-ieee", "crc32", "c32"}, filehash.CRC32_IEEE),
+		hashPrintCommand("CRC64-ISO", []string{"crc64-iso"}, filehash.CRC64_ISO),
+		hashPrintCommand("CRC64-ECMA", []string{"crc64-ecma"}, filehash.CRC64_ECMA),
+		hashPrintCommand("Adler-32", []string{"adler-32", "adler", "adl"}, filehash.Adler32),
+		hashPrintCommand("FNV-32", []string{"fnv-32"}, filehash.FNV32),
+		hashPrintCommand("FNV-32a", []string{"fnv-32a"}, filehash.FNV32a),
+		hashPrintCommand("FNV-64", []string{"fnv-64"}, filehash.FNV64),
+		hashPrintCommand("FNV-64a", []string{"fnv-64a"}, filehash.FNV64a),
+	}
+
+	app.Commands = commands
+
+	app.Run(os.Args)
+}
---
+++ filehash.go
@@ -0,0 +1,122 @@
+package filehash
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
+	"hash/adler32"
+	"hash/crc32"
+	"hash/crc64"
+	"hash/fnv"
+	"io/ioutil"
+
+	"golang.org/x/crypto/sha3"
+)
+
+// Calc returns the hash value of the file
+func Calc(hash hash.Hash, file string) []byte {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return []byte{}
+	}
+	hash.Reset()
+	hash.Write(data)
+	return hash.Sum(nil)
+}
+
+// SHA1 returns the SHA1 hash value of the file
+func SHA1(file string) []byte {
+	return Calc(sha1.New(), file)
+}
+
+// SHA224 returns the SHA224 hash value of the file
+func SHA224(file string) []byte {
+	return Calc(sha256.New224(), file)
+}
+
+// SHA256 returns the SHA256 hash value of the file
+func SHA256(file string) []byte {
+	return Calc(sha256.New(), file)
+}
+
+// SHA512 returns the SHA-512 hash value of the file
+func SHA512(file string) []byte {
+	return Calc(sha512.New(), file)
+}
+
+// SHA512_224 returns the SHA-512/224 hash value of the file
+func SHA512_224(file string) []byte {
+	return Calc(sha512.New512_224(), file)
+}
+
+// SHA512_256 returns the SHA-512/256 hash value of the file
+func SHA512_256(file string) []byte {
+	return Calc(sha512.New512_256(), file)
+}
+
+// SHA3_224 returns the SHA3-224 hash value of the file
+func SHA3_224(file string) []byte {
+	return Calc(sha3.New224(), file)
+}
+
+// SHA3_256 returns the SHA3-256 hash value of the file
+func SHA3_256(file string) []byte {
+	return Calc(sha3.New256(), file)
+}
+
+// SHA3_384 returns the SHA3-384 hash value of the file
+func SHA3_384(file string) []byte {
+	return Calc(sha3.New384(), file)
+}
+
+// SHA3_512 returns the SHA3-512 hash value of the file
+func SHA3_512(file string) []byte {
+	return Calc(sha3.New512(), file)
+}
+
+// MD5 returns the MD5 hash value of the file
+func MD5(file string) []byte {
+	return Calc(md5.New(), file)
+}
+
+// CRC32_IEEE returns the CRC-32 hash value of the file using the IEEE polynomial
+func CRC32_IEEE(file string) []byte {
+	return Calc(crc32.NewIEEE(), file)
+}
+
+// CRC64_ISO returns the CRC-64 hash value of the file using the ISO polynomial
+func CRC64_ISO(file string) []byte {
+	return Calc(crc64.New(crc64.MakeTable(crc64.ISO)), file)
+}
+
+// CRC64_ECMA returns the CRC-64 hash value of the file using the ECMA polynomial
+func CRC64_ECMA(file string) []byte {
+	return Calc(crc64.New(crc64.MakeTable(crc64.ECMA)), file)
+}
+
+// Adler32 returns the Adler-32 hash value of the file
+func Adler32(file string) []byte {
+	return Calc(adler32.New(), file)
+}
+
+// FNV32 returns the 32-bit FNV-1 hash value of the file
+func FNV32(file string) []byte {
+	return Calc(fnv.New32(), file)
+}
+
+// FNV32a returns the 32-bit FNV-1a hash value of the file
+func FNV32a(file string) []byte {
+	return Calc(fnv.New32a(), file)
+}
+
+// FNV64 returns the 64-bit FNV-1 hash value of the file
+func FNV64(file string) []byte {
+	return Calc(fnv.New64(), file)
+}
+
+// FNV64a returns the 64-bit FNV-1a hash value of the file
+func FNV64a(file string) []byte {
+	return Calc(fnv.New64a(), file)
+}
